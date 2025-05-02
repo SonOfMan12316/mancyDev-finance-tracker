64b02ebd5db36c3dd8d602a5e41abf336c7aac59
SonOfMan12316 <mancycarter@gmail.com>
2025-05-02 20:32:47 +0100

    built popover component retouched confirm dialog component too

---
+++ src/components/global/ConfirmDialog.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement } from "react";
+import Modal from "./Modal";
+import { Button } from "../ui/Button/Button";
+
+interface ConfirmDialogProps {
+  isOpen: boolean;
+  title: string | ReactElement;
+  zIndex?: 30 | 40;
+  message?: string;
+  confirmText?: string;
+  cancelText?: string;
+  onCancel?: () => void;
+}
+
+const ConfirmDialog: React.FC<ConfirmDialogProps> = (props) => {
+  const { isOpen, zIndex, title, message, confirmText, onCancel, cancelText } =
+    props;
+  return (
+    <Modal
+      zIndex={zIndex ? zIndex : 30}
+      isOpen={isOpen}
+      title={title}
+      onClose={onCancel}
+    >
+      <div className="">
+        <div className="my-3">
+          <h1 className="text-ch-grey text-xs font-normal">{message}</h1>
+        </div>
+        <div className="flex flex-col items-center mt-5">
+          <Button
+            variant="destroy"
+            size="sm"
+            aria-label="cancel dialog"
+            onClick={onCancel}
+          >
+            {cancelText}
+          </Button>
+          <Button
+            size="sm"
+            variant="tertiary"
+            className="font-normal md:pt-4"
+            aria-label="confirm dialog"
+          >
+            {confirmText}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmDialog;
---
+++ src/components/global/Popover.tsx
@@ -0,0 +1,67 @@
+import React, { ReactNode } from "react";
+import { Button } from "../ui/Button/Button";
+import { ButtonVariant } from "../ui/Button/Button/Button";
+import classnames from "classnames";
+import { useClickOutside } from "../../hooks/useClickOutside";
+
+interface PopoverProps {
+  isOpen?: boolean;
+  setIsOpen: (newState: boolean) => void;
+  trigger: ReactNode;
+  triggerVariant?: ButtonVariant;
+  children: ReactNode;
+  className?: string;
+  contentPositionY?: "top" | "bottom";
+  contentPositionX?: "left" | "right";
+  bg?: string;
+  hover?: boolean;
+}
+
+const Popover: React.FC<PopoverProps> = ({
+  isOpen,
+  setIsOpen,
+  trigger,
+  triggerVariant = "tertiary",
+  children,
+  className,
+  contentPositionY = "bottom",
+  contentPositionX = "left",
+  bg,
+  hover,
+}) => {
+  const popoverRef = useClickOutside(() => {
+    setIsOpen(false);
+  });
+  return (
+    <div className={`w-fit relative ${className}`} ref={popoverRef}>
+      <Button
+        variant={triggerVariant}
+        className="inline-block"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {trigger}
+      </Button>
+      {isOpen && (
+        <div
+          className={classnames(
+            `absolute z-20 bg-${
+              bg ? bg : "white"
+            } rounded-lg transition-all duration-300 ease-in-out px-4`,
+            {
+              "right-0": contentPositionX === "left",
+              "left-0": contentPositionX === "right",
+              "top-8": contentPositionY === "bottom",
+              "h-fit w-max max-w-[14rem] md:max-w-[16rem] pt-0 ": isOpen,
+              "h-0 w-0 overflow-hidden": !isOpen,
+              "shadow-budget-popover": !hover,
+            }
+          )}
+        >
+          {children}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Popover;
