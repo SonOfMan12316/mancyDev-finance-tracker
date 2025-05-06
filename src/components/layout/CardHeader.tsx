7137db6cef77bf7a9aee99bc497722d4e6aae49d
SonOfMan12316 <mancycarter@gmail.com>
2025-05-06 15:55:21 +0100

    refactored CardHeader props to give way for pots popover

--- src/components/layout/CardHeader.tsx
+++ src/components/layout/CardHeader.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Popover from "../global/Popover";
 import { Ellipsis } from "../icons";
-import useUIStore from "../../store/ui-store";
-import { budgetInterface } from "../../types/global";
 
 interface CardHeaderProps {
   progressColor: string;
   title: string;
   popOpen: boolean;
   setPopOpen: (newState: boolean) => void;
-  budget?: budgetInterface[];
+  type: "budget" | "pot";
+  onEdit: () => void;
+  onDelete: () => void;
 }
 
 const CardHeader: React.FC<CardHeaderProps> = ({
@@ -17,35 +17,36 @@ const CardHeader: React.FC<CardHeaderProps> = ({
   title,
   popOpen,
   setPopOpen,
-  budget,
-}) => {
-  const { setOpenModal, setSelectedBudget } = useUIStore();
-
+  type,
+  onEdit,
+  onDelete,
+}: CardHeaderProps) => {
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center gap-x-2">
-        <div className={`w-5 h-5 rounded-full bg-${progressColor}`}></div>
+        <div className={`w-5 h-5 rounded-full bg-ch-${progressColor}`}></div>
         <h1 className="font-bold text-lg capitalize">{title}</h1>
       </div>
       <Popover trigger={<Ellipsis />} isOpen={popOpen} setIsOpen={setPopOpen}>
         <ul className="py-3.5">
-          <li
-            onClick={() => {
-              const budgetToEdit =
-                budget?.find((item) => item.category === title) || null;
-              setOpenModal({ type: "edit", data: title });
-              setSelectedBudget(budgetToEdit);
-            }}
-            className="border-b border-ch-light-grey text-sm font-normal text-black cursor-pointer pb-2.5"
-          >
-            Edit Budget
-          </li>
-          <li
-            onClick={() => setOpenModal({ type: "delete", data: title })}
-            className="text-sm font-normal text-ch-red cursor-pointer pt-2.5"
-          >
-            Delete Budget
-          </li>
+          {type === "budget" && (
+            <>
+              <li
+                onClick={() => {
+                  onEdit();
+                }}
+                className="border-b border-ch-light-grey text-sm font-normal text-black cursor-pointer pb-2.5"
+              >
+                Edit Budget
+              </li>
+              <li
+                onClick={() => onDelete()}
+                className="text-sm font-normal text-ch-red cursor-pointer pt-2.5"
+              >
+                Delete Budget
+              </li>
+            </>
+          )}
         </ul>
       </Popover>
     </div>
