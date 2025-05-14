8be1c1f156db2582c9964c8f18bab261a8923d82
SonOfMan12316 <mancycarter@gmail.com>
2025-05-14 20:21:43 +0100

    updated progressBar component to allow multiple progress bar

--- src/components/global/ProgressBar.tsx
+++ src/components/global/ProgressBar.tsx
@@ -1,26 +1,36 @@
 interface ProgressBarProps {
   value: number;
+  secondValue: number;
   height?: string;
   innerHeight: string;
   backgroundColor?: string;
   progressColor?: string;
+  calculatedProgressColor?: string;
 }
 
 const ProgressBar: React.FC<ProgressBarProps> = ({
   value,
+  secondValue,
   height = "",
   innerHeight = "",
   backgroundColor = "bg-ch-yellow",
   progressColor = "green",
+  calculatedProgressColor,
 }) => {
   return (
     <div
-      className={`w-full ${height} ${backgroundColor} flex items-center rounded-lg overflow-hidden px-1`}
+      className={`w-full ${height} ${backgroundColor} flex gap-x-0.5 items-center rounded-lg overflow-hidden`}
     >
       <div
-        className={`bg-ch-${progressColor} ${innerHeight} rounded-lg transition-all duration-300`}
+        className={`bg-ch-${progressColor} ${innerHeight} rounded-l transition-all duration-300`}
         style={{ width: `${Math.min(Math.max(value, 0), 100)}%` }}
       ></div>
+      {calculatedProgressColor && (
+        <div
+          className={`bg-ch-${calculatedProgressColor} ${innerHeight} rounded-r transition-all duration-300`}
+          style={{ width: `${Math.min(Math.max(secondValue, 0), 100)}%` }}
+        ></div>
+      )}
     </div>
   );
 };
