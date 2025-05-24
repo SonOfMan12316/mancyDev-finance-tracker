778d5586d521eeed81bd315eed5e0f74361c7640
SonOfMan12316 <mancycarter@gmail.com>
2025-05-24 23:41:38 +0100

    added filtering transaction with category, updated loading states, empty data response to users

--- src/components/pages/transaction.tsx
+++ src/components/pages/transaction.tsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import { db } from "../../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  query,
+  onSnapshot,
+  where,
+  Query,
+} from "firebase/firestore";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 import { Search, DropdownIcon } from "../icons";
@@ -15,6 +22,7 @@ import { PAGE } from "../../utils/global";
 
 interface TransactionInterface {
   transaction: transactionInterface[];
+  isLoading?: boolean;
 }
 
 const column = ["Recipient / Sender", "Category", "Transaction Date", "Amount"];
@@ -35,30 +43,65 @@ export const Transaction = () => {
 
   const transactionCollectionRef = collection(db, "transactions");
 
-  const getTransactions = async () => {
-    try {
-      setIsLoading(true);
-      const data = await getDocs(transactionCollectionRef);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      })) as transactionInterface[];
-      setTransactions(filteredData);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-    }
-  };
+  useEffect(() => {
+    const getTransaction = async () => {
+      try {
+        setIsLoading(true);
+        const snapshot = await getDocs(transactionCollectionRef);
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as transactionInterface[];
+        setTransactions(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Initial load error:", error);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getTransaction();
+  }, []);
 
   useEffect(() => {
-    getTransactions();
-  }, []);
+    let queryRef: Query = transactionCollectionRef;
+
+    const categoryValue =
+      selectedCategoryOption?.value || selectedCategoryOption;
+
+    if (categoryValue && categoryValue !== "All transactions") {
+      queryRef = query(
+        transactionCollectionRef,
+        where("category", "==", categoryValue)
+      );
+    }
+    setIsLoading(true);
+    const unsubscribe = onSnapshot(
+      queryRef,
+      (snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as transactionInterface[];
+        setTransactions(data);
+        setIsLoading(false);
+      },
+      (error) => {
+        console.error("Real-time error:", error);
+        setIsLoading(false);
+      }
+    );
+
+    return () => unsubscribe();
+  }, [selectedCategoryOption, selectedSortOption]);
 
   return (
     <div className="w-screen">
       <Layout title="transactions">
         <div className="px-4 md:px-8">
-          <div className="bg-white min-h-screen p-4 md:p-8 rounded-md w-full">
+          <div className="bg-white min-h-90 p-4 md:p-8 rounded-md w-full">
             <div className="flex justify-between items-center w-full gap-4">
               <div className="flex-1">
                 <Input
@@ -99,23 +142,13 @@ export const Transaction = () => {
                 </div>
               </div>
             </div>
-            {isLoading ? (
-              <div className="absolute w-full top-0 left-0 right-0 bottom-0 flex justify-center items-center z-50">
-                <div className="h-1/4 w-1/4">
-                  <DotLottieReact
-                    src="https://lottie.host/dfbd7ad5-7401-4b5f-8e39-f57f325c38c9/myKWeEicAa.lottie"
-                    loop
-                    autoplay
-                  />
-                </div>
-              </div>
-            ) : (
-              <>
-                <TransactionTable transaction={currentItems} />
-                <TransactionCard transaction={currentItems} />
-              </>
-            )}
-            {transactions.length > 0 && (
+
+            <TransactionTable
+              transaction={currentItems}
+              isLoading={isLoading}
+            />
+            <TransactionCard transaction={currentItems} />
+            {transactions && transactions.length > 0 && (
               <Pagination
                 currentPage={pageNumber}
                 pageSize={pageSize}
@@ -130,74 +163,113 @@ export const Transaction = () => {
   );
 };
 
-const TransactionTable: React.FC<TransactionInterface> = ({ transaction }) => {
+const TransactionTable: React.FC<TransactionInterface> = ({
+  transaction,
+  isLoading,
+}) => {
   return (
-    <table className="py-4 mt-8 mb-4 table-fixed hidden md:table">
-      <thead>
-        <tr>
-          {column.map((i, index) => {
-            return (
-              <th
-                className="text-left text-ch-grey text-sm font-normal font-publicSans border-b border-ch-grey/0.15 pb-2"
-                key={index}
-              >
-                {i}
-              </th>
-            );
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {transaction.map((txn, index) => (
-          <tr
-            className={`${
-              index !== transaction.length - 1
-                ? "border-b border-ch-grey/0.15"
-                : ""
-            }`}
-            key={index}
-          >
-            <td className="w-2/4 items-center py-4 ">
-              <div className="flex items-center gap-x-4">
-                <img className="w-10 h-10 rounded-full" src={txn.avatar} />
-                <h1 className="whitespace-nowrap font-bold text-sm">
-                  {txn.name}
-                </h1>
-              </div>
-            </td>
-            <td className="w-1/4 ">
-              <span className="text-left text-ch-grey text-sm font-normal font-publicSans">
-                {txn.category}
-              </span>
-            </td>
-            <td className="w-1/4 ">
-              <span className="text-left text-ch-grey text-sm font-normal font-publicSans">
-                {toDMYString(txn.date)}
-              </span>
-            </td>
-            <td className="w-1/4">
-              <h1
-                className={`${
-                  txn.amount < 0 ? "text-black" : "text-ch-green"
-                } font-bold text-base`}
-              >
-                {txn.amount < 0
-                  ? txn.amount.toLocaleString("en-US", {
-                      style: "currency",
-                      currency: "USD",
-                    })
-                  : "+" +
-                    txn.amount.toLocaleString("en-US", {
-                      style: "currency",
-                      currency: "USD",
-                    })}
-              </h1>
-            </td>
-            <td className="border-b border-ch-grey/0.15"></td>
+    <div>
+      <table className="py-4 mt-8 mb-4 table-fixed hidden md:table">
+        <thead>
+          <tr>
+            {column.map((i, index) => {
+              const widths = ["w-2/4", "w-1/4", "w-1/4", "w-1/4"];
+              return (
+                <th
+                  className={`text-left text-ch-grey text-sm font-normal font-publicSans border-b border-ch-grey/0.15 pb-2 ${widths[index]}`}
+                  key={index}
+                >
+                  {i}
+                </th>
+              );
+            })}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="w-full relative">
+          {isLoading ? (
+            <tr>
+              <td colSpan={4} className="text-center py-4">
+                <DotLottieReact
+                  src="https://lottie.host/dfbd7ad5-7401-4b5f-8e39-f57f325c38c9/myKWeEicAa.lottie"
+                  loop
+                  autoplay
+                  className="w-2/3 h-2/3 mx-auto"
+                />
+                <p className="text-ch-black text-lg font-normal">Loading ...</p>
+              </td>
+            </tr>
+          ) : transaction && transaction.length > 0 && !isLoading ? (
+            transaction.map((txn, index) => {
+              return (
+                <tr
+                  className={`${
+                    index !== transaction.length - 1
+                      ? "border-b border-ch-grey/0.15"
+                      : ""
+                  }`}
+                  key={index}
+                >
+                  <td className="w-2/4 items-center py-4 ">
+                    <div className="flex items-center gap-x-4">
+                      <img
+                        className="w-10 h-10 rounded-full"
+                        src={txn.avatar}
+                      />
+                      <h1 className="whitespace-nowrap font-bold text-sm">
+                        {txn.name}
+                      </h1>
+                    </div>
+                  </td>
+                  <td className="w-1/4 ">
+                    <span className="text-left text-ch-grey text-sm font-normal font-publicSans">
+                      {txn.category}
+                    </span>
+                  </td>
+                  <td className="w-1/4 ">
+                    <span className="text-left text-ch-grey text-sm font-normal font-publicSans">
+                      {toDMYString(txn.date)}
+                    </span>
+                  </td>
+                  <td className="w-1/4">
+                    <h1
+                      className={`${
+                        txn.amount < 0 ? "text-black" : "text-ch-green"
+                      } font-bold text-base`}
+                    >
+                      {txn.amount < 0
+                        ? txn.amount.toLocaleString("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                          })
+                        : "+" +
+                          txn.amount.toLocaleString("en-US", {
+                            style: "currency",
+                            currency: "USD",
+                          })}
+                    </h1>
+                  </td>
+                  <td className="border-b border-ch-grey/0.15"></td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan={4} className="text-center py-4">
+                <DotLottieReact
+                  src="https://lottie.host/dfbd7ad5-7401-4b5f-8e39-f57f325c38c9/myKWeEicAa.lottie"
+                  loop
+                  autoplay
+                  className="w-2/3 h-2/3 mx-auto"
+                />
+                <p className="text-ch-black text-base font-norma">
+                  No transactions found.
+                </p>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
