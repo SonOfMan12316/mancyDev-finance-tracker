a2d05078106d0b548902516fdb8f0679097b16fd
SonOfMan12316 <mancycarter@gmail.com>
2025-06-07 22:08:50 +0100

    added if budget exist handling

--- src/api/resource/budget.ts
+++ src/api/resource/budget.ts
@@ -1,17 +1,27 @@
-import { writeBatch, doc, collection } from "firebase/firestore";
+import { writeBatch, doc, collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
 import { budgetInfo } from "../../types/global";
 import toast from "react-hot-toast";
 
 export const createBudget = async (data: budgetInfo) => {
+  const budgetRef = collection(db, "budgets");
+  const querySnapshot = await getDocs(budgetRef);
+
+  const budgetExist = querySnapshot.docs.find(
+    (doc) => doc.data().category === data.category
+  );
+
+  if (budgetExist) {
+    toast.error(`${data.category} budget already exists`);
+  } else {
     const batch = writeBatch(db);
-    const budgetRef = collection(db, 'budgets')
-    batch.set(doc(budgetRef), data)
+    batch.set(doc(budgetRef), data);
 
     try {
-        await batch.commit();
-        return { success: true }
+      await batch.commit();
+      return { success: true };
     } catch (error) {
-        toast.error("Error whilst creating budget")
+      toast.error("Error whilst creating budget");
     }
-}
+  }
+};
