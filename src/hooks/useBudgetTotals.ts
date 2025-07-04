2741039685289ae30c269d29d7183560d9b819c6
SonOfMan12316 <mancycarter@gmail.com>
2025-07-04 23:56:43 +0100

    seperated themes handling for pot and budget

--- src/hooks/useBudgetTotals.ts
+++ src/hooks/useBudgetTotals.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import { budgetInfo } from "../types/global";
+import { budgetInfo, potInfo } from "../types/global";
 
 interface BudgetTotals {
   limit: number;
@@ -8,14 +8,21 @@ interface BudgetTotals {
     amountSpent: number;
     color: string;
   }>;
-  usedThemes: Set<string>;
+  usedPotThemes: Set<string>;
+  usedBudgetThemes: Set<string>;
   usedCategories: Set<string>;
+  usedPotName: Set<string>;
 }
 
-const useBudgetTotals = (budgets: budgetInfo[]): BudgetTotals => {
+const useBudgetTotals = (
+  budgets?: budgetInfo[],
+  pots?: potInfo[]
+): BudgetTotals => {
   return useMemo(() => {
-    const usedThemes = new Set<string>();
+    const usedBudgetThemes = new Set<string>();
+    const usedPotThemes = new Set<string>();
     const usedCategories = new Set<string>();
+    const usedPotName = new Set<string>();
     const totals = {
       limit: 0,
       amountSpent: 0,
@@ -23,11 +30,13 @@ const useBudgetTotals = (budgets: budgetInfo[]): BudgetTotals => {
         amountSpent: number;
         color: string;
       }>,
-      usedThemes,
-      usedCategories
+      usedBudgetThemes,
+      usedPotThemes,
+      usedCategories,
+      usedPotName,
     };
 
-    if (budgets?.length > 0) {
+    if (budgets && budgets?.length > 0) {
       totals.limit = budgets.reduce((sum, b) => sum + Number(b.maximum), 0);
       totals.amountSpent = budgets.reduce(
         (sum, b) => sum + Number(b.amount_spent),
@@ -35,7 +44,7 @@ const useBudgetTotals = (budgets: budgetInfo[]): BudgetTotals => {
       );
 
       totals.categories = budgets.map((budget) => {
-        usedThemes.add(budget.theme);
+        usedBudgetThemes.add(budget.theme);
         usedCategories.add(budget.category);
 
         return {
@@ -46,8 +55,15 @@ const useBudgetTotals = (budgets: budgetInfo[]): BudgetTotals => {
       });
     }
 
+    if (pots && pots?.length > 0) {
+      pots?.map((pot) => {
+        usedPotThemes.add(pot.theme);
+        usedPotName.add(pot.name);
+      });
+    }
+
     return totals;
-  }, [budgets]);
+  }, [budgets, pots]);
 };
 
 export default useBudgetTotals;
