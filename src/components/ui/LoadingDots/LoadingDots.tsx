cdc65f94d5be34a548ab9b03120559529274116f
SonOfMan12316 <mancycarter@gmail.com>
2025-07-19 00:05:32 +0100

    skipped LoadingDots component

---
+++ src/components/ui/LoadingDots/LoadingDots.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import classnames from 'classnames';
+
+import s from './LoadingDots.module.css';
+
+interface LoadingDotsProps {
+  size?: 'default' | 'sm'
+}
+
+const LoadingDots: FC<LoadingDotsProps> = ({ size = 'default' }) => {
+
+  return (
+    <span className={s.root}>
+      {
+        [...Array(3)].map((dot: unknown, index: number) => (
+          <span className={classnames(s.dot, {[s.smallDot]: size === 'sm'})} key={`dot_${index + 1}`} />
+        ))
+      }
+    </span>
+  );
+};
+
+export default LoadingDots;
