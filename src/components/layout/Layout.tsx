acceadf6bcc75f5ee9df7e6cfc23fa5048859de6
SonOfMan12316 <mancycarter@gmail.com>
2025-09-30 07:09:41 +0100

    fixed alignment issues on mobile

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { SideNav, Header, BottomNav } from "../global";
-import { HeaderProps } from "../global/Header";
+import React from 'react'
+import { SideNav, Header, BottomNav } from '../global'
+import { HeaderProps } from '../global/Header'
 
 interface LayoutProps {
-  children: React.ReactNode;
-  displayHeader?: boolean;
+  children: React.ReactNode
+  displayHeader?: boolean
 }
 
 const Layout: React.FC<LayoutProps & HeaderProps> = ({
@@ -17,28 +17,26 @@ const Layout: React.FC<LayoutProps & HeaderProps> = ({
 }) => {
   return (
     <div className="min-h-screen flex bg-ch-beige">
-  <SideNav />
-  <div className="flex-1 flex flex-col h-screen">
-    <div className="flex-none">
-      <Header
-        title={title}
-        className={className}
-        displayButton={displayButton}
-        buttonTitle={buttonTitle}
-        onClick={onClick}
-      />
-    </div>
-    <div className="flex-1 overflow-y-auto pb-20 md:pb-28 lg:pb-6">
-      <div className="mt-4">
-        {children}
+      <SideNav />
+      <div className="flex-1 flex flex-col h-screen">
+        <div className="flex-none">
+          <Header
+            title={title}
+            className={className}
+            displayButton={displayButton}
+            buttonTitle={buttonTitle}
+            onClick={onClick}
+          />
+        </div>
+        <div className="flex-1 overflow-y-auto pb-16 lg:pb-6">
+          <div className="mt-4">{children}</div>
+        </div>
+        <div className="fixed bottom-0 left-0 w-full lg:hidden z-50">
+          <BottomNav />
+        </div>
       </div>
     </div>
-    <div className="fixed bottom-0 left-0 w-full lg:hidden z-50">
-      <BottomNav />
-    </div>
-  </div>
-</div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
